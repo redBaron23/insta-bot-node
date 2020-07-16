fb3df5c9c6d6ecdd99cc4a77a97c5097d71d9361
redbaron23 <patotomastoledo6@gmail.com>
2020-07-15 23:24:04 -0300

    testing

---
+++ test.js
@@ -0,0 +1,88 @@
+var express = require('express')
+
+var path = require('path')
+
+var appDir = path.dirname(require.main.filename);
+
+
+
+const instaJs = require(appDir+'/api/instaJs.js')
+
+var bodyParser = require('body-parser')
+var app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.route('/followUserFollowers')
+  .get(async function(req, res) {
+    res.send("No paper");
+  })
+  .post(async function(req, res) {
+    
+    let result; 
+    const username = req.body.username
+    const password = req.body.password
+    result = (username &&  password) ? (await instaJs.followUserFollowers(username,password)) : "No username or password provided"
+    console.log(result)
+    res.send(result);
+  })
+app.route('/unfollowSession')
+  .get(async function(req, res) {
+    res.send("No paper");
+  })
+  .post(async function(req, res) {
+    
+    let result; 
+    const username = req.body.username
+    const password = req.body.password
+    result = (username &&  password) ? (await instaJs.unfollowSession(username,password)) : "No username or password provided"
+    console.log(result)
+    res.send(result);
+  })
+
+
+
+app.route('/unfollowGarcas')
+  .get(async function(req, res) {
+    res.send("No paper");
+  })
+  .post(async function(req, res) {
+    
+    let result; 
+    const username = req.body.username
+    const password = req.body.password
+    result = (username &&  password) ? (await instaJs.unfollowGarcas(username,password)) : "No username or password provided"
+    console.log(result)
+    res.send(result);
+  })
+
+
+
+
+
+app.route('/farmFamous')
+  .get(async function(req, res) {
+    res.send("No paper");
+  })
+  .post(async function(req, res) {
+    
+    let result; 
+    const username = req.body.username
+    const password = req.body.password
+    result = (username &&  password) ? (await instaJs.farmFamous(username,password)) : "No username or password provided"
+    console.log(result)
+    res.send(result);
+  })
+  .put(function(req, res) {
+    res.send('Update the book');
+  });
+
+
+
+
+app.listen(1112, function (){
+	console.log("Listen on 1112")
+});
