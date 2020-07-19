11cce0890ca54049a11a8a9469cb79e63c90b53a
redbaron23 <patotomastoledo6@gmail.com>
2020-07-19 02:47:53 -0300

    New wait times

--- api/instaJs.js
+++ api/instaJs.js
@@ -14,11 +14,6 @@ const FAMOUS_URI = appDir+'/api/data/accountFamous.json'
 
 const accountHelper = require(appDir+'/api/accountHelper')
 
-const _username = 'redbaron397'
-const _password = 'Sega1072_'
-//Helper account
-let _account = new accountHelper.Account(_username,_password);
-_account.init();
 
 const bounces = 10000;
 
@@ -29,7 +24,6 @@ async function farmFamous(USERNAME,PASSWORD){
   const ACCOUNTS_FAMOUS = JSON.parse(rawdata)
    let _status
   try{
-   //let garcas = await accountHelper.getGarcas('pato.toledo',patoWhitelist)
     let response = {};
     let account = new accountHelper.Account(USERNAME,PASSWORD);
     await account.init()
@@ -115,7 +109,7 @@ async function followAll(account,userName){
     const ratio = 0.2
 
     const rSize = 500 //Number of users per request (lower better to don't get a ban)
-    const totalFollowers = await _account.countFollowers(userName);
+    const totalFollowers = await account.countFollowers(userName);
     const times = Math.trunc(totalFollowers / rSize)+1
     //const totalFollowing = await account.countFollowing(userName);
     console.log('Going to follow ~'+String(totalFollowers).red+' from '+userName.green+' in '+String(times).blue+' times')
@@ -125,7 +119,7 @@ async function followAll(account,userName){
       console.log(String(i+'/'+times).red)
       console.log('At time: '+ await helper.dateTime())
 
-      let followers = await _account.getUserFollowers(userName,rSize)
+      let followers = await account.getUserFollowers(userName,rSize)
       
       
       //Just follow users
@@ -136,16 +130,22 @@ async function followAll(account,userName){
     } 
 }
 async function isViable(userName,ratio){
-  
+  let response
   const realRatio = (ratio) ? ratio : 0.23
-  const followers = await _account.countFollowers(userName);
-  const following = await _account.countFollowing(userName);
-  
-  const currentRatio = following/followers
+  try{
 
-  const _status = (currentRatio >= realRatio)
-  
-  return _status
+    const following = await account.countFollowing(userName);
+    const followers = await account.countFollowers(userName);
+    const currentRatio = following/followers
+    response = (currentRatio >= realRatio)
+  }
+  catch(e){
+    console.log('Error found -> viable')
+    response = true
+  }
+  finally{
+    return response
+  }
 
 }
 async function bounceAccounts(account,bounces,ACCOUNTS_FAMOUS){
@@ -155,7 +155,6 @@ async function bounceAccounts(account,bounces,ACCOUNTS_FAMOUS){
   let timeBounce = 0;
   let timeBetween = 0;
   let used = 0;
-
   for (i = 0; i < bounces; i++) {
     console.log('Bounce number: '+colors.red(i) )
     //Follow all accounts
@@ -189,7 +188,7 @@ async function unfollowAccounts(account,ACCOUNTS_FAMOUS){
       await account.unfollow(userName)
     }
     catch(e){
-      console.log('Account not followed')
+      console.log('Account not unfollowed')
     }
     finally{
     //timeout
@@ -200,8 +199,11 @@ async function unfollowAccounts(account,ACCOUNTS_FAMOUS){
 }
 async function followAccounts(account,ACCOUNTS_FAMOUS,ratio){
   
-  const MIN_TIME = 300000; //5min
-  const MAX_TIME = 420005; //7min
+  const MIN_TIME = 300000/3; //5min
+  const MAX_TIME = 420005/3; //7min
+  const QUERYs = 20
+  const QUERY_MIN_TIME = (5*60*1000)
+  const QUERY_MAX_TIME = (15*60*1000)
   
   let timeout = 0;
   let i = 1
@@ -210,6 +212,10 @@ async function followAccounts(account,ACCOUNTS_FAMOUS,ratio){
 	   if(!userName){
 	      console.log('Vacio en la posicion'+i)
 	   }
+    // if( !(i % QUERYs) && (i>=20)){
+       //Sleep after 20 querys
+     // await helper.sleepRandom(QUERY_MIN_TIME,QUERY_MAX_TIME)
+      //}
     console.log('Following: ' + (i +'/'+ ACCOUNTS_FAMOUS.length +' '+userName).green)
     console.log('At time: '+ await helper.dateTime())
     try{ 
@@ -220,6 +226,7 @@ async function followAccounts(account,ACCOUNTS_FAMOUS,ratio){
       }
     }
     catch(e){
+      console.log(e)
       console.log('Account not followed')
     }
     finally{
