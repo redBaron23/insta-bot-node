f3a953a985a82eba1c7c80a8f6c0726bdd1cb4bd
pato <pato>
2020-08-09 18:13:31 -0300

    bugfix

--- api/accountHelper.js
+++ api/accountHelper.js
@@ -61,7 +61,6 @@ async function getCookies(page, USERNAME) {
       session: false
     });
   }
-  console.log(cookies.length);
   console.log(cookies);
   if (existCsrftoken && existSessionid) {
     console.log("Session created");
@@ -103,9 +102,9 @@ async function logIn(USERNAME, PASSWORD) {
     await btn.evaluate(btn => btn.click());
     //To get cookies
     await page.waitForSelector(INSTAGRAM_LOGO, { timeout: 5000 });
-    //const res = await getCookies(page, USERNAME);
+    const res = await getCookies(page, USERNAME);
     console.log("Logged Successful()");
-    return "c"; //res;
+    return res;
   } catch (e) {
     let reason;
     const ERROR_TEXT = "#slfErrorAlert";
@@ -185,7 +184,7 @@ class Account {
       }
     } catch (e) {
       let res;
-      if (e.includes("password")) {
+      if (e.message.includes("password")) {
         res = e;
       } else {
         console.log(e);
